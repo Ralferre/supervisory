9ba9ce6fceac17fa4e3e379efe1a25f9e39e7b22
Ralferre <raphael.l.ferreira@hotmail.com>
2024-04-15 18:00:30 -0300

    Creating new Java application project

---
+++ src/main/java/com/example/supervisory/SupervisoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.supervisory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupervisoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupervisoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/supervisory/SupervisoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supervisory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervisoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
