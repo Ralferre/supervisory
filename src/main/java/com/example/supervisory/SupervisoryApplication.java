a44823f59b33c1cada51694f34a4678f37fce1fe
Ralferre <raphael.l.ferreira@hotmail.com>
2024-04-16 19:18:00 -0300

    Inserting generator password method

--- src/main/java/com/example/supervisory/SupervisoryApplication.java
+++ src/main/java/com/example/supervisory/SupervisoryApplication.java
@@ -3,11 +3,14 @@ package com.example.supervisory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static dto.model.user.PasswordGenerator.generatePassword;
+
 @SpringBootApplication
 public class SupervisoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupervisoryApplication.class, args);
+
 	}
 
 }
